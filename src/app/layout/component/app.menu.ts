c32357d534eaff639576c0ddc8643dbfd6fe45c0
TRamadan <tarek123ahm@gmail.com>
2026-02-26 15:50:07 +0200

    Refine show doctor section with additional enhancements and optimizations

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -20,194 +20,194 @@ export class AppMenu {
 
     ngOnInit() {
         const sideRoutesString = localStorage.getItem('sideRoutes');
-        // if (sideRoutesString) {
-        //     const sideRouts = JSON.parse(sideRoutesString);
+        if (sideRoutesString) {
+            const sideRouts = JSON.parse(sideRoutesString);
 
-        //     this.model = sideRouts
-        //     ?.filter((route: any) => route.children?.length)
-        //     .map((route: any) => ({
-        //         label: route.name,
-        //         items: route.children.map((child: any) => ({
-        //             label: child.name,
-        //             icon: '',
-        //             routerLink: [child.pageUrl]
-        //         }))
-        //     }));
-        // }
+            this.model = sideRouts
+                ?.filter((route: any) => route.children?.length)
+                .map((route: any) => ({
+                    label: route.name,
+                    items: route.children.map((child: any) => ({
+                        label: child.name,
+                        icon: '',
+                        routerLink: [child.pageUrl]
+                    }))
+                }));
+        }
 
-        this.model = [
-            {
-                label: 'Home',
-                items: [
-                    {
-                        label: 'Dashboard',
-                        icon: 'pi pi-fw pi-home',
-                        routerLink: ['/']
-                    }
-                ]
-            },
-            {
-                label: 'User management',
-                items: [
-                    {
-                        label: 'Add user',
-                        icon: '',
-                        routerLink: ['/uikit/adduser']
-                    },
-                    {
-                        label: 'Add group',
-                        icon: '',
-                        routerLink: ['/uikit/addgroup']
-                    },
-                    {
-                        label: 'Add roles and permissions',
-                        icon: '',
-                        routerLink: ['/uikit/addpermission']
-                    }
-                    // {
-                    //     label: 'Add Permission',
-                    //     icon: ''
-                    // }
-                ]
-            },
+        // this.model = [
+        //     {
+        //         label: 'Home',
+        //         items: [
+        //             {
+        //                 label: 'Dashboard',
+        //                 icon: 'pi pi-fw pi-home',
+        //                 routerLink: ['/']
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         label: 'User management',
+        //         items: [
+        //             {
+        //                 label: 'Add user',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/adduser']
+        //             },
+        //             {
+        //                 label: 'Add group',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/addgroup']
+        //             },
+        //             {
+        //                 label: 'Add roles and permissions',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/addpermission']
+        //             }
+        //             // {
+        //             //     label: 'Add Permission',
+        //             //     icon: ''
+        //             // }
+        //         ]
+        //     },
 
-            {
-                label: 'Doctor',
-                items: [
-                    { label: 'Add service', icon: '', routerLink: ['/uikit/services'] },
-                    { label: 'Add location', icon: '', routerLink: ['/uikit/location'] },
-                    { label: 'Appointments', icon: '', routerLink: ['/uikit/appointments'] },
-                    { label: 'Add Patient report', icon: '', routerLink: ['/uikit/patientreport'] },
-                    {
-                        label: 'Add treatment plan',
-                        icon: '',
-                        routerLink: ['/uikit/phases-sessions']
-                    },
+        //     {
+        //         label: 'Doctor',
+        //         items: [
+        //             { label: 'Add service', icon: '', routerLink: ['/uikit/services'] },
+        //             { label: 'Add location', icon: '', routerLink: ['/uikit/location'] },
+        //             { label: 'Appointments', icon: '', routerLink: ['/uikit/appointments'] },
+        //             { label: 'Add Patient report', icon: '', routerLink: ['/uikit/patientreport'] },
+        //             {
+        //                 label: 'Add treatment plan',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/phases-sessions']
+        //             },
 
-                    {
-                        label: 'Tracking session attendance',
-                        icon: '',
-                        routerLink: ['/uikit/trackingattendance']
-                    },
-                    {
-                        label: 'Working hours',
-                        icon: '',
-                        routerLink: ['/uikit/workinghours']
-                    },
-                    {
-                        label: 'Calendar',
-                        icon: '',
-                        routerLink: ['/uikit/calendar']
-                    },
-                    {
-                        label: 'specialities',
-                        icon: '',
-                        routerLink: ['/uikit/specialities']
-                    }
-                ]
-            },
-            {
-                label: 'Coach',
-                items: [
-                    {
-                        label: 'session management',
-                        icon: '',
-                        routerLink: ['/uikit/sessionmanagement']
-                    }
-                ]
-            },
-            {
-                label: 'Coach manager',
-                items: [
-                    {
-                        label: 'coach management',
-                        icon: '',
-                        routerLink: ['/uikit/coachmanagement']
-                    }
-                ]
-            },
-            {
-                label: 'Website control',
-                items: [
-                    // {
-                    //     label: 'Hero Section Config',
-                    //     icon: '',
-                    //     routerLink: ['/uikit/herosectionconfig']
-                    // },
-                    {
-                        label: 'our super stars',
-                        icon: '',
-                        routerLink: ['/uikit/superstars']
-                    },
-                    {
-                        label: 'Our methodology section config',
-                        icon: '',
-                        routerLink: ['/uikit/Methodology']
-                    },
-                    {
-                        label: 'Our benefits section config',
-                        icon: '',
-                        routerLink: ['/uikit/benefits']
-                    },
-                    {
-                        label: 'Knowledge hub page config',
-                        icon: '',
-                        routerLink: ['/uikit/knowledgehub']
-                    },
-                    {
-                        label: 'Our solutions section config',
-                        icon: '',
-                        routerLink: ['/uikit/oursolutions']
-                    },
-                    {
-                        label: 'Our partners section config',
-                        icon: '',
-                        routerLink: ['/uikit/partners']
-                    },
+        //             {
+        //                 label: 'Tracking session attendance',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/trackingattendance']
+        //             },
+        //             {
+        //                 label: 'Working hours',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/workinghours']
+        //             },
+        //             {
+        //                 label: 'Calendar',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/calendar']
+        //             },
+        //             {
+        //                 label: 'specialities',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/specialities']
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         label: 'Coach',
+        //         items: [
+        //             {
+        //                 label: 'session management',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/sessionmanagement']
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         label: 'Coach manager',
+        //         items: [
+        //             {
+        //                 label: 'coach management',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/coachmanagement']
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         label: 'Website control',
+        //         items: [
+        //             // {
+        //             //     label: 'Hero Section Config',
+        //             //     icon: '',
+        //             //     routerLink: ['/uikit/herosectionconfig']
+        //             // },
+        //             {
+        //                 label: 'our super stars',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/superstars']
+        //             },
+        //             {
+        //                 label: 'Our methodology section config',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/Methodology']
+        //             },
+        //             {
+        //                 label: 'Our benefits section config',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/benefits']
+        //             },
+        //             {
+        //                 label: 'Knowledge hub page config',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/knowledgehub']
+        //             },
+        //             {
+        //                 label: 'Our solutions section config',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/oursolutions']
+        //             },
+        //             {
+        //                 label: 'Our partners section config',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/partners']
+        //             },
 
-                    {
-                        label: 'Success Stories section config',
-                        icon: '',
-                        routerLink: ['/uikit/successstories']
-                    },
+        //             {
+        //                 label: 'Success Stories section config',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/successstories']
+        //             },
 
-                    // {
-                    //     label: 'Contact us section config',
-                    //     icon: '',
-                    //     routerLink: ['/uikit/contactus']
-                    // },
-                    // {
-                    //     label: 'About us config',
-                    //     icon: '',
-                    //     routerLink: ['/uikit/aboutus']
-                    // },
-                    {
-                        label: 'Our team',
-                        icon: '',
-                        routerLink: ['/uikit/our-team']
-                    }
-                ]
-            },
-            {
-                label: 'Measurements configuration',
-                items: [
-                    {
-                        label: 'measurements configuration',
-                        icon: '',
-                        routerLink: ['/uikit/measurements-config']
-                    }
-                ]
-            }
-            // {
-            //     label: 'Reports and forms configuration',
-            //     items: [
-            //         {
-            //             label: 'reports',
-            //             icon: '',
-            //             routerLink: ['/uikit/reportconfig']
-            //         }
-            //     ]
-            // }
-        ];
+        //             // {
+        //             //     label: 'Contact us section config',
+        //             //     icon: '',
+        //             //     routerLink: ['/uikit/contactus']
+        //             // },
+        //             // {
+        //             //     label: 'About us config',
+        //             //     icon: '',
+        //             //     routerLink: ['/uikit/aboutus']
+        //             // },
+        //             {
+        //                 label: 'Our team',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/our-team']
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         label: 'Measurements configuration',
+        //         items: [
+        //             {
+        //                 label: 'measurements configuration',
+        //                 icon: '',
+        //                 routerLink: ['/uikit/measurements-config']
+        //             }
+        //         ]
+        //     }
+        //     // {
+        //     //     label: 'Reports and forms configuration',
+        //     //     items: [
+        //     //         {
+        //     //             label: 'reports',
+        //     //             icon: '',
+        //     //             routerLink: ['/uikit/reportconfig']
+        //     //         }
+        //     //     ]
+        //     // }
+        // ];
     }
 }
--- src/app/pages/uikit/calendar/services/calendar.service.ts
+++ src/app/pages/uikit/calendar/services/calendar.service.ts
@@ -10,22 +10,37 @@ export class CalendarService {
 
     constructor(private http: HttpClient) { }
 
+    /**
+     * Fetch all doctors that work at the given location within the specified date range.
+     * @param locationId  ID of the selected location
+     * @param fromDate    Start date (YYYY-MM-DD)
+     * @param toDate      End date (YYYY-MM-DD)
+     */
+    getDoctorsByLocation(locationId: number, fromDate: string, toDate: string) {
+        const params = new HttpParams()
+            .set('locationId', locationId)
+            .set('fromDate', fromDate)
+            .set('toDate', toDate);
+
+        return this.http.get(`${this.apiUrl}WorkingHours/available-doctors`, { params });
+    }
+
     //here is the function needed to get all calender data
-    getAllCalenderData(date: string, slotMinutes: number = 30, doctorId?: number, locationId?: number, endDate?: string) {
+    getAllCalenderData(date: string, slotMinutes: number = 15, doctorId?: number, locationId?: number, endDate?: string) {
         let params = new HttpParams()
-            .set('date', date)
-            .set('slotMinutes', slotMinutes);
+            .set('DateFrom', date)
+            .set('SlotMinutes', slotMinutes);
 
         if (doctorId) {
-            params = params.set('doctorId', doctorId);
+            params = params.set('DoctorId', doctorId);
         }
 
         if (locationId) {
-            params = params.set('locationId', locationId);
+            params = params.set('LocationId', locationId);
         }
 
         if (endDate) {
-            params = params.set('toDate', endDate);
+            params = params.set('DateTo', endDate);
         }
 
         return this.http.get(`${this.apiUrl}Appointments/Calendar`, { params });
